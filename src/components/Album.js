825bf3535d7015af9a228f349d4604e2be014341
Klaudia Kieszkowska <claudiaandjazz@gmail.com>
2018-08-08 15:50:56 +0200

    Styling albums page

--- src/components/Album.js
+++ src/components/Album.js
@@ -3,53 +3,23 @@ import Lightbox from './Lightbox';
 import Photo from'./Photo';
 
 let food = [
-    '/img/food/1.jpg',
-    '/img/food/2.jpg',
-    '/img/food/3.jpg',
-    '/img/food/4.jpg',
-    '/img/food/5.jpg',
-    '/img/food/6.jpg',
-    '/img/food/7.jpg',
-    '/img/food/8.jpg',
-    '/img/food/9.jpg',
+    '/img/food/1.jpg', '/img/food/2.jpg', '/img/food/3.jpg', '/img/food/4.jpg', '/img/food/5.jpg', '/img/food/6.jpg',
+    '/img/food/7.jpg', '/img/food/8.jpg', '/img/food/9.jpg',
 ];
 let interior = [
-    './img/interior/1.jpg',
-    './img/interior/2.jpg',
-    './img/interior/3.jpg',
-    './img/interior/4.jpg',
-    './img/interior/5.jpg',
-    './img/interior/6.jpg',
-    './img/interior/7.jpg',
-    './img/interior/8.jpg',
-    './img/interior/9.jpg',
-    './img/interior/10.jpg',
+    './img/interior/1.jpg', './img/interior/2.jpg', './img/interior/3.jpg', './img/interior/4.jpg',
+    './img/interior/5.jpg', './img/interior/6.jpg', './img/interior/7.jpg', './img/interior/8.jpg',
+    './img/interior/9.jpg', './img/interior/10.jpg',
 ];
 let landscape = [
-    './img/landscape/1.jpg',
-    './img/landscape/2.jpg',
-    './img/landscape/3.jpg',
-    './img/landscape/4.jpg',
-    './img/landscape/5.jpg',
-    './img/landscape/6.jpg',
-    './img/landscape/7.jpg',
-    './img/landscape/8.jpg',
-    './img/landscape/9.jpg',
-    './img/landscape/10.jpg',
-    './img/landscape/11.jpg',
-    './img/landscape/12.jpg',
+    './img/landscape/1.jpg', './img/landscape/2.jpg', './img/landscape/3.jpg', './img/landscape/4.jpg',
+    './img/landscape/5.jpg', './img/landscape/6.jpg', './img/landscape/7.jpg', './img/landscape/8.jpg',
+    './img/landscape/9.jpg', './img/landscape/10.jpg', './img/landscape/11.jpg', './img/landscape/12.jpg',
 ];
 let portraits = [
-    './img/portraits/1.jpg',
-    './img/portraits/2.jpg',
-    './img/portraits/3.jpg',
-    './img/portraits/4.jpg',
-    './img/portraits/5.jpg',
-    './img/portraits/6.jpg',
-    './img/portraits/7.jpg',
-    './img/portraits/8.jpg',
-    './img/portraits/9.jpg',
-    './img/portraits/10.jpg',
+    './img/portraits/1.jpg', './img/portraits/2.jpg', './img/portraits/3.jpg', './img/portraits/4.jpg',
+    './img/portraits/5.jpg', './img/portraits/6.jpg', './img/portraits/7.jpg', './img/portraits/8.jpg',
+    './img/portraits/9.jpg', './img/portraits/10.jpg',
 ];
 let galleries = [food, interior, landscape, portraits];
 
@@ -92,16 +62,12 @@ class Album extends Component {
                 <div className='row'>
                     { this.galleries.titles.map((el, i) => {
                         return (
-                            <div className='col-3' key={ i }>
-                                <div className='card' onClick={() => this.openGallery(i)}>
-                                    <img className='card-img-top'
-                                         src={ process.env.PUBLIC_URL + this.galleries.photos[i][0] }
-                                         alt={ el + ' cover'}
-                                    />
-                                    <div className='card-body'>
-                                        <h4 className='card-title'>{ el }</h4>
-                                    </div>
-                                </div>
+                            <div className='col-3 albumCover' key={ i } onClick={() => this.openGallery(i)}>
+                                <img className='img-fluid cover'
+                                     src={ process.env.PUBLIC_URL + this.galleries.photos[i][0] }
+                                     alt={ el + ' cover'}
+                                />
+                                <span className='p-2'>{ el }</span>
                             </div>
                         );
                     }) }
@@ -118,7 +84,7 @@ class Album extends Component {
                         >
                             <span className='h1' aria-hidden="true">&times;</span>
                         </button>
-                        
+
                         <h2 className='col-11'>{ this.galleries.titles[this.state.gallery] }</h2>
                         { this.galleries.photos[this.state.gallery].map((el, i) => {
                             return (
